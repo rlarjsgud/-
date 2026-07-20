903d452c304704e363d459672054168c056d0f59
rlarjsgud <aa01086284849@gmail.com>
2026-07-20 13:39:51 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ st.title("카운터 앱")
 if 'count' not in st.session_state:
     st.session_state.count = 0
 if st.button("증가"):
-    st.session_state.count += ㄱ
+    st.session_state.count +=119298575778737372525948759437598437593759843759847598745943875492074912
 st.markdown(f"## 현재 숫자: `{st.session_state.count}`")
