810d57ba0d47c7c11671708a9dda34d01510dc7c
rlarjsgud <aa01086284849@gmail.com>
2026-07-20 14:43:06 +0900

    Correct end time check in app.py
    Fix condition to check end time before calculating duration.

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ with col1:
         st.session.end = 0          # 종료 시간 초기화
 with col2:
     if st.button("종료"):
-        if st.session_state.start_time != 0:
+        if st.session_state.end_time != 0:
             ?? = time.time()
             # 걸린 시간 계산 (종료 시간 - 시작 시간)
             st.session_state.result = st.session.end - st.session.start
