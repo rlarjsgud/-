29047cc1948bae684ecff7fe47477b87e3f0a018
rlarjsgud <aa01086284849@gmail.com>
2026-07-20 13:39:29 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title("카운터 앱")
 if 'count' not in st.session_state:
-    st.session_state.count = ㄱ
+    st.session_state.count = 0
 if st.button("증가"):
-    st.session_state.count += 1
+    st.session_state.count += ㄱ
 st.markdown(f"## 현재 숫자: `{st.session_state.count}`")
