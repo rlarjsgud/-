2d66624cf44d516373f20d6ac3bc2867e1a7bfb1
rlarjsgud <aa01086284849@gmail.com>
2026-07-20 11:19:07 +0900

    Update UI labels in app.py

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 import streamlit as st
 
-st.markdown("# AI 챗봇 만들기")
+st.markdown("# 앱 UI만들기")
 st.markdown("---")
 st.markdown("## 질문을 하시면 AI 친구가 응답합니다.")
 st.header("1. 기본 정보 입력")
-user_id = st.text_input("아이디(ID)를 입력하세요", placeholder="example_user")
-age = st.number_input("나이를 입력하세요", min_value=1, max_value=100, value=17)
-question = st.text_area("AI에게 보낼 질문을 입력하세요", placeholder="여기에 질문을 작성해 주세요.")
+user_id = st.text_input("이름", placeholder="example_user")
+
 
 st.header("2. 챗봇 설정")
 ai_model = st.radio("학년", ["1", "2", "3"], horizontal=True)
