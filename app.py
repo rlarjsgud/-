539c0410bcabaf00fe74971fed64e604b8c86a4e
rlarjsgud <aa01086284849@gmail.com>
2026-07-20 11:22:27 +0900

    Fix slider labels for creativity and difficulty

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ user_id = st.text_input("이름", placeholder="이름")
 
 ai_model = st.radio("학년", ["1", "2", "3"], horizontal=True)
 tone = st.selectbox("반", ["1", "2", "3"])
-features = st.multiselect("난이도", ["쉬움", "보통", "어려움",])
-creativity = st.slider("AI의 창의성 수준을 설정하세요", 0, 100, 50)
+
+creativity = st.slider("난이도", 0, 50, 100)
 ai_speed = st.select_slider("점수",options=["0", "25", "50", "75", "100"],value="50")
 
 st.markdown("---")
