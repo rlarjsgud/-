05cb644fa7f9f9a6ffd6f110c83d10fb43ff2c31
rlarjsgud <aa01086284849@gmail.com>
2026-07-20 12:55:32 +0900

    Fix formatting in app.py

--- app.py
+++ app.py
@@ -8,4 +8,4 @@ with st.sidebar:
     st.info(f"반가워요,{user_name}님! 오늘 날씨는 '{weather}이네요.")
 
 st.title("AI 코디 메이커")
-st.write("사이드바에서 날씨를 먼저 선택하고 코디를 시작하세요!)
+st.write("사이드바에서 날씨를 먼저 선택하고 코디를 시작하세요!")
