4bbf366eb0e41d3d66f0e831646f280ebc8814e4
rlarjsgud <aa01086284849@gmail.com>
2026-07-21 12:05:04 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ st.title("2의 제곱수 카운트")
 if 'count' not in st.session_state:
     st.session_state.count = 2
 
-if st.button("증가"):
+if st.button("X2"):
     st.session_state.count += int(st.session_state.count)*1
 
 st.markdown(f"## 현재숫자: {st.session_state.count}")
