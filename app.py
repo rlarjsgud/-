f453c58b23197e10c17291e0ec251db8f6309b01
rlarjsgud <aa01086284849@gmail.com>
2026-07-21 11:59:17 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.title("카운터 앱")
 
 if 'count' not in st.session_state:
-    st.session_state.count = 0
+    st.session_state.count = 2
 
 if st.button("증가"):
     st.session_state.count += int(st.session_state.count)*2
